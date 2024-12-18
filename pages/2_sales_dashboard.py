0792209a2231b2f4c803abe9ac5cbc3c016fba8c
Pallavi Patil <96570104+pallaviwale@users.noreply.github.com>
2024-12-18 15:45:22 +0530

    Update 2_sales_dashboard.py

--- pages/2_sales_dashboard.py
+++ pages/2_sales_dashboard.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import Preprocessor_sales_dashboard
+from Codes import Preprocessor_sales_dashboard
 from typing import List, Tuple
 import matplotlib.pyplot as plt
 import seaborn as sns
